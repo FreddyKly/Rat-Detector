398b50dc114e67e7c90c2701735b56978a07ffb1
FreddyKly <Frederik.Kliemt@web.de>
2022-11-11 01:21:05 +0100

    Changed Layout

--- RasPiCluster/client/src/components/HelloWorld.vue
+++ RasPiCluster/client/src/components/HelloWorld.vue
@@ -1,42 +1,66 @@
 <template>
-  <div class="container">
-    <h1>Latest Detection</h1>
-    <hr>
-    <p class="error" v-if="error">{{ error }}</p>
-    <div class="detections-container">
-      <div class="detections" 
-      v-for="(detection, index) in detections"
-      v-bind:item="detection"
-      v-bind:index="index"
-      v-bind:key="detection._id">
-      {{ `${detection.createdAt.getDate()}.${detection.createdAt.getMonth()}.${detection.createdAt.getFullYear()}` }}
-        <p class="text">{{ detection.text }}</p>
-      </div>
+  <v-app id="inspire">
+    <v-app-bar app shrink-on-scroll>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <div class="container">
+      <h1>Latest Detections</h1>
+      <hr />
+      <p class="error" v-if="error">{{ error }}</p>
     </div>
-  </div>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col
+            v-for="(detection, index) in detections"
+            v-bind:item="detection"
+            v-bind:index="index"
+            v-bind:key="detection._id"
+          >
+            <v-card height="200">
+              <v-card-text>
+                {{
+                  `${detection.createdAt.toUTCString()}`
+                }}
+                <p class="text">{{ detection.text }}</p></v-card-text
+              >
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import detectionsService from '@/detectionsService';
+import detectionsService from "@/detectionsService";
 
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   data() {
     return {
       detections: [],
-      error: '',
-      text: ''
-    }
+      error: "",
+      text: "",
+    };
   },
   async created() {
-    try{
+    try {
       this.detections = await detectionsService.getDetections();
-      console.log(this.detections)
-    } catch(err){
+      console.log(this.detections);
+    } catch (err) {
       this.error = err.message;
     }
-  }
-}
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
