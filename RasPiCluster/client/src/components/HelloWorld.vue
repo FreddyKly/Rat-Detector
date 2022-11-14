eb18233f82518e0bf92cb8e771e7148f9ba145ff
FreddyKly <Frederik.Kliemt@web.de>
2022-11-14 22:12:49 +0100

    hovering works

--- RasPiCluster/client/src/components/HelloWorld.vue
+++ RasPiCluster/client/src/components/HelloWorld.vue
@@ -12,23 +12,38 @@
     <v-main>
       <v-container>
         <v-row>
-          <v-col v-for="(detection, index) in detections" v-bind:item="detection" v-bind:index="index"
-            v-bind:key="detection._id" cols="2">
-            <v-hover v-slot="{hover}">
-              <v-card height="250" width="250" :elevation="hover ? 2 : 10" :class="{ 'on-hover': hover }">
-                <v-img :src="`data:image/jpg;base64,${detection.image}`" class="align-end">
-                  <v-card-title class="text-white"> {{ detection.createdAt }} </v-card-title>
-                </v-img>
-                <!-- <v-card-text>
+          <v-col v-for="(detection, index) in detections" 
+          v-bind:item="detection" 
+          v-bind:index="index"
+            v-bind:key="detection._id" 
+            cols="2">
+            <v-hover>
+              <template 
+              v-slot:default="{ isHovering, props }">
+                <v-card 
+                height="250" 
+                width="250" 
+                :elevation="isHovering ? 15 : 1" 
+                v-bind="props">
+                  <v-img :src="`data:image/jpg;base64,${detection.image}`" class="align-end" v-bind="props">
+                    <v-card-title class="text-white"> {{ detection.createdAt }} </v-card-title>
+                  </v-img>
+                  <!-- <v-card-text>
                 {{ `${detection.createdAt.toUTCString()}` }}
                 <p class="text">{{ detection.text }}</p>
               </v-card-text> -->
-              </v-card>
+                </v-card>
+              </template>
             </v-hover>
-
           </v-col>
         </v-row>
       </v-container>
+      <v-hover>
+          <template v-slot:default="{ isHovering, props }">
+            <v-card v-bind="props" :color="isHovering ? 'primary' : undefined" title="Hover over me" text="...">
+            </v-card>
+          </template>
+        </v-hover>
     </v-main>
   </v-app>
 </template>
